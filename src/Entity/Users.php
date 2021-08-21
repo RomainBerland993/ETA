1f19791018af47a56bcff5e47332b3b3143466de
Romain <romainberland03@gmail.com>
2021-08-21 21:07:56 +0200

    auth et inscription

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -6,11 +6,13 @@ use App\Repository\UsersRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -102,6 +104,11 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $likes;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
+    
     public function __construct()
     {
         $this->events = new ArrayCollection();
@@ -417,6 +424,18 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
                 $like->setUsers(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
